4c839dd314b2cc0a5206a8b737e9fc3705a5b808
daniel-llach <daniel.llach@gmail.com>
2019-09-06 10:28:07 -0400

    Added resume component with highlight right answers

--- trivia/app/components/ResumeRow/index.js
+++ trivia/app/components/ResumeRow/index.js
@@ -1,16 +1,47 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {StyleSheet, Text, View} from 'react-native'
 import {connect} from '../../store'
 import he from 'he'
 
 const ResumeRow = ({item, success}) => {
   return (
-    <View>
-      <Text>{success ? '+' : '-'}</Text>
-      <Text>{he.decode(item.question)}</Text>
+    <View style={styles.row}>
+      <Text style={[styles.icon, success && styles.green]}>
+        {success ? '+' : '-'}
+      </Text>
+      <Text style={[styles.question, success && styles.green]}>
+        {he.decode(item.question)}
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  icon: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'grey',
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignSelf: 'flex-start',
+  },
+  question: {
+    width: '90%',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'grey',
+    textAlign: 'left',
+    paddingRight: 20,
+  },
+  green: {
+    color: 'green',
+  },
+})
 
 export default connect(({questions, score}) => ({
   questions,
